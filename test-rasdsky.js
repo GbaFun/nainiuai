ba3be48ec146fc9b197b37f77e6f3c50c04a1a06
RasdSky <rasdsky@qq.com>
2024-12-19 15:17:28 +0800

    测试读表

--- test-rasdsky.js
+++ test-rasdsky.js
@@ -1,8 +1,20 @@
 
 function doTest() {
-    const cfg = GM_getResourceText("data");
-    console.log(cfg);
+    const job = "死灵";
+    const level = 14;
     $.getJSON("https://raw.githubusercontent.com/GbaFun/IdleinfinityTools/refs/heads/main/data.json", function (data) {
         console.log(data);
+        $.each(data[0], function (infoIndex, info) {
+            var cfg = info[job];
+            console.log(cfg);
+            if (!cfg.isNaN) {
+                $.each(cfg, function (infoIndex, info2) {
+                    console.log(info2);
+                    if (info2[Lv].min <= level && level < info2[Lv].max) {
+                        console.log(info2.主手);
+                    }
+                })
+            }
+        })
     });
 }
