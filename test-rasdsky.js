5cf5416767e668a8ee7cf1d8080f2210d494055f
RasdSky <rasdsky@qq.com>
2024-12-19 14:58:41 +0800

    修改配置

--- test-rasdsky.js
+++ test-rasdsky.js
@@ -1,6 +1,8 @@
 
 function doTest() {
-    $.getJSON("data.json", function (data) {
+    const cfg = GM_getResourceText("data");
+    console.log(cfg);
+    $.getJSON("https://raw.githubusercontent.com/GbaFun/IdleinfinityTools/refs/heads/main/data.json", function (data) {
         console.log(data);
     });
 }
