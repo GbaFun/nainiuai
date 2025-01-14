c5858c81812223856e254dfe9fddefa6c06f780c
RasdSky <rasdsky@qq.com>
2025-01-14 10:25:46 +0800

    优化log格式

--- IdleAuto/IdleAuto/Scripts/Manager/FileLogManager.cs
+++ IdleAuto/IdleAuto/Scripts/Manager/FileLogManager.cs
@@ -10,12 +10,29 @@ public enum emLogType
     Debug = 0,
     AutoEquip = 1,
     AhScan = 2,
+    Error = 3,
+    Warning = 4,
 }
 public static class P
 {
     public static void Log(string message, emLogType logType = emLogType.Debug)
     {
-        Console.WriteLine($"{logType}_log:{message}");
+        string lable = string.Empty;
+        switch (logType)
+        {
+            case emLogType.Error:
+                lable = "错!!!!误："; break;
+            case emLogType.Warning:
+                lable = "警!!!!告："; break;
+            case emLogType.AutoEquip:
+                lable = "一键修车："; break;
+            case emLogType.AhScan:
+                lable = "自动扫拍："; break;
+            default:
+                lable = "消!!!!息："; break;
+        }
+        //Console.ForegroundColor = color;
+        Console.WriteLine($"{lable}:{message}");
 
         if (logType == emLogType.Debug)
         {
